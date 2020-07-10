787907493466e24ddd9ae3f44912b3cf6e5ed2c9
trampboy495 <histroylover@gmail.com>
2020-07-10 13:38:19 +0530

    [siddhesh] add.UC2 Flips coin multiple times and displays no of occurences of Heads and Tails

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,22 @@
 #!/bin/bash -x
 Heads=1
 Tails=0
-r=$((RANDOM % 2))
-if [ $r -eq $Heads ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+NoOfWinsForHeads=0
+NoOfWinsForTails=0
+NoOfLoops=0
+read -p "enter the no of times to loop" n
+while [ $NoOfLoops -lt $n ]
+do
+	r=$((RANDOM % 2))
+	if [ $r -eq $Heads ]
+	then
+		echo "Heads"
+		((NoOfWinsForHeads++))
+	else
+		echo "Tails"
+		((NoOfWinsForTails++))
+	fi
+	((NoOfLoops++))
+done
+echo "no of times Head Won is $NoOfWinsForHeads"
+echo "no of times Head Won is $NoOfWinsForTails"
